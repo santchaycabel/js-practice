9eb05c3317534d443509afae4fad6c7dc93bbbc8
santchaycabel <santchaycabel18@gmail.com>
2025-03-09 18:27:20 +0800

    Update

---
+++ numbers.js
@@ -0,0 +1,19 @@
+// 1. Create variables for your favorite numbers.
+let firstFavNum = 7;  
+let secondFavNum = 3; 
+
+// 2. Perform arithmetic operations.
+let addition = firstFavNum + secondFavNum;
+let subtraction = firstFavNum - secondFavNum;
+let multiplication = firstFavNum * secondFavNum;
+let division = firstFavNum / secondFavNum;
+let modulus = firstFavNum % secondFavNum;
+let exponentiation = firstFavNum ** secondFavNum;
+
+// 3. Log the results to the console.
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+console.log("Modulus:", modulus);
+console.log("Exponentiation:", exponentiation);
